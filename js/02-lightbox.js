c36dea8a8516ead858fc332f114a455ae3372d05
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-08-04 19:33:14 +0300

    task 2 completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryItemsContainer = document.querySelector('.gallery');
+const CardsMarkup = createGaleryCardsMarkup(galleryItems);
+
+galleryItemsContainer.insertAdjacentHTML('beforeend', CardsMarkup);
+
+function createGaleryCardsMarkup(gallery) {
+    return gallery.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    }).join('');  
+    
+};
+
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+gallery.on('show.simplelightbox');
